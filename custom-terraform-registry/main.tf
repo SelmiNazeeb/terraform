94aa9e9f6c0e6f3eec505c36329611bd625c31e5
SelmiNazeeb <135132441+SelmiNazeeb@users.noreply.github.com>
2025-05-06 17:17:15 +0530

    Create main.tf

---
+++ custom-terraform-registry/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2-instance" {
+  source  = "SelmiNazeeb/ec2-instance/aws"
+  version = "1.0.0"
+}
