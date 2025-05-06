34af965f24b0e8469b28d6fb12753128dfcc5943
SelmiNazeeb <135132441+SelmiNazeeb@users.noreply.github.com>
2025-05-06 16:03:36 +0530

    Create variable.tf

---
+++ ec2-with-variableFile/variable.tf
@@ -0,0 +1,15 @@
+# Define variables
+variable "my_ami_id" {
+    type = string
+}
+variable "my_instance_type" {
+    type = string
+}
+
+variable "my_region" {
+    type = string
+}
+
+variable "my_ec2" {
+    type = string
+}
