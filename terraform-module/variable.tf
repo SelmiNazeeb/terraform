2367f2b3673c45aa45a97f3df8e93314a744e80b
SelmiNazeeb <135132441+SelmiNazeeb@users.noreply.github.com>
2025-05-06 16:08:43 +0530

    Create variable.tf

---
+++ terraform-module/variable.tf
@@ -0,0 +1,13 @@
+variable "my_ami_id_input" {
+    type = string
+    default = "ami-0f88e80871fd81e91" 
+}
+variable "my_instance_type_input" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "my_bucket_name_input" {
+    type = string
+    default = "selmi1234"
+}
