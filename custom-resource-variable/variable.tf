be8a86cf635ff175ddeb0f305f51087afa586159
SelmiNazeeb <135132441+SelmiNazeeb@users.noreply.github.com>
2025-05-06 16:54:32 +0530

    Create variable.tf

---
+++ custom-resource-variable/variable.tf
@@ -0,0 +1,35 @@
+variable "vpc_cidr" {
+    type = string
+}
+
+variable "pub_cidr" {
+    type = string
+}
+
+variable "priv_cidr" {
+    type = string
+}
+
+variable "az1" {
+    type = string
+}
+
+variable "az2" {
+    type = string
+}
+
+variable "my_ami" {
+    type = string
+}
+
+variable "my_instance_type" {
+    type = string
+}
+
+variable "my_pub_instance_name" {
+    type = string
+}
+
+variable "my_priv_instance_name" {
+    type = string
+}
