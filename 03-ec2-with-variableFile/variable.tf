a0012dc6d98579fc0865616cf86779921b3ce161
SelmiNazeeb <135132441+SelmiNazeeb@users.noreply.github.com>
2025-08-14 15:15:47 +0530

    Update variable.tf

--- 03-ec2-with-variableFile/variable.tf
+++ 03-ec2-with-variableFile/variable.tf
@@ -13,3 +13,15 @@ variable "my_region" {
 variable "my_ec2" {
     type = string
 }
+
+
+############################################
+###without tfvars
+variable "my-ami" {
+  type = string
+  default = "ami-0f88e80871fd81e91"
+}
+variable "my-instance-type" {
+  type = string
+  default = "t2.micro"
+}
